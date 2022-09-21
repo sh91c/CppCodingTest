27631da878a6b8712eb0e44b4a66262c0cb3cd9b
choi <sh91cxp@gmail.com>
2022-09-22 01:41:32 +0900

    BOJ 10808

---
+++ BOJ/10808/my_sol.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+char a[26];
+int cnt[26] = {0};
+string s;
+
+int main(void)
+{
+    for (int i = 97; i < 97 + 26; i++)
+    {
+        a[i - 97] = (char) i;
+    }
+
+    cin >> s;
+
+    for (char c : s)
+    {
+        cnt[(int) c - 97] += 1;
+    }
+
+    for (int i: cnt) cout << i << " ";
+}
