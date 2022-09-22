ac5c1e6599dad98281e85729c30df97f1c62a3f2
choi <sh91cxp@gmail.com>
2022-09-23 07:13:54 +0900

    BOJ 10808 2979 10988

---
+++ BOJ/week1/2_10808/my_sol_2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a[26];
+string str;
+
+int main(void)
+{
+    cin >> str;
+    for (char c : str) a[c - 'a'] += 1;
+    for (int i : a) cout << i << " ";
+}
